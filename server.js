7135d853b10be0bc142d8892d5fefd83a3cd76a5
Daniel Liu <daniel@eieihome.com>
2016-04-13 16:38:38 -0400

    bin

--- server.js
+++ server.js
@@ -17,46 +17,57 @@ var C = {};
 //     });
 // });
 
-function read(s){
-	return new Promise(function(resolve, reject){
-		fs.readdir( s, function( error, data ) {
-	        if ( error ) {
-	            reject( error );
-	        } else {
-	            resolve( data );
-	        }
-	    });
-	});
-};
+// function read(s){
+// 	// return Promise.promisify()
+// 	return new Promise(function(resolve, reject){
+// 		fs.readdir( s, function( error, data ) {
+// 	        if ( error ) {
+// 	            reject( error );
+// 	        } else {
+// 	            resolve( data );
+// 	        }
+// 	    });
+// 	});
+// };
 
-read(srcpath)
-.then(
-	function (items) {
-	    items.map(function(item,i) {
-	        if(fs.statSync(item).isDirectory() && item.includes('folder')){
-	        	read(item)
-	        	.then(
-	        		function(c){
-	        	  		C[item] = c;
-		    			console.log(c);
-	        		},
-	        		function(e){
+// read(srcpath)
+// .then(
+// 	function (items) {
+// 	    items.map(function(item,i) {
+// 	        if(fs.statSync(item).isDirectory() && item.includes('folder')){
+// 	        	read(item)
+// 	        	.then(
+// 	        		function(c){
+// 	        	  		C[item] = c;
+// 		    			console.log(c);
+// 	        		},
+// 	        		function(e){
 	        	  
-	        		}
-	        	);
-	        }
-	    });
-	},
-	function(error){
+// 	        		}
+// 	        	);
+// 	        }
+// 	    });
+// 	    console.log('end!!end');
+// 	},
+// 	function(error){
 
-	}
-)
-.then(
-	function(c){
-  		C[item] = c;
-		console.log(c);
-	},
-	function(e){
+// 	}
+// )
+// .then(
+// 	function(c){
+// 		console.log('end!!');
+// 	},
+// 	function(e){
   
+// 	}
+// )
+fs.readdirSync(srcpath).map(function(item,i) {
+	if(fs.statSync(item).isDirectory() && item.includes('folder')){
+		fs.readdirSync(item).map(function(p,i){
+			C[p] = !!!C[p] ? 1 : C[p] + 1;
+		});
 	}
-)
+});
+for(var pp in C){
+	console.log(pp + ':' + C[pp])
+}
